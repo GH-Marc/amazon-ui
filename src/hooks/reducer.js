a07d4fe4bceeacb238058a4018de0062bb8eabe4
Marc <marcelopgm65@gmail.com>
2020-12-24 16:12:24 -0300

    :sparkles: Reducer hook

---
+++ src/hooks/reducer.js
@@ -0,0 +1,48 @@
+export const initialState = {
+  basket: [],
+  user: null
+}
+
+export const getBasketTotal = (basket) =>
+  basket?.reduce((quantity, item) => quantity + item.price, 0);
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'ADD_TO_BASKET':
+      return {
+        ...state,
+        basket: [...state.basket, action.item],
+      };
+
+    case 'REMOVE_FROM_BASKET':
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+
+      let newBasket = [...state.basket];
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `O produto ${action.id} não pode ser removido, pois não está no carrinho!`
+        )
+      }
+
+      return {
+        ...state,
+        basket: newBasket
+      }
+
+    case 'SET_USER':
+      return {
+        ...state,
+        user: action.user
+      }
+    
+    default:
+      return state;
+  }
+};
+
+export default reducer;
