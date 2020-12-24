39a00c189140591a082c7362114f1d36210a73df
Marc <marcelopgm65@gmail.com>
2020-12-24 16:11:47 -0300

    :sparkles: Checkout Products component

---
+++ src/components/CheckoutProduct/index.js
@@ -0,0 +1,38 @@
+import './styles.css';
+
+import { useStateValue } from '../../hooks/StateProvider';
+
+function CheckoutProduct({ id, image, title, price, rating}) {
+  const [{ basket }, dispatch] = useStateValue();
+  const removeFromBasket = () => {
+    dispatch({
+      type: 'REMOVE_FROM_BASKET',
+      id: id,
+    });
+  }
+
+  return (
+    <div className="checkoutProduct">
+      <img className="checkoutProduct__image" src={image} alt="product_image"/>
+
+      <div className="checkoutProduct__info">
+        <p className="checkoutProduct__title">{title}</p>
+        <p className="checkoutProduct__price">
+          <small>R$</small>
+          <strong>{price}</strong>
+        </p>
+
+        <div className="checkoutProduct__rating">
+          {Array(rating)
+          .fill().map((item, index) => (
+            <p>⭐️</p>
+          ))}
+        </div>
+
+        <button onClick={removeFromBasket}>Remover do carrinho</button>
+      </div>
+    </div>
+  );
+}
+
+export default CheckoutProduct;
