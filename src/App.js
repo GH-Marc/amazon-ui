bcd2153baa5d01120515369dfbde0e4a74217e22
Marc <marcelopgm65@gmail.com>
2020-12-24 16:07:46 -0300

    :wrench: chore: Adds login page & user auth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
+import { useEffect } from 'react';
+
 import './App.css';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Home from './components/Home';
+import Login from './components/Login';
 import Checkout from './components/Checkout';
 
+import { auth } from './firebase';
+import { useStateValue } from './hooks/StateProvider';
+
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      } else {
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  }, [dispatch]);
+
   return (
     <Router>
       <div className="app">
-        <Header />
 
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
 
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>        
