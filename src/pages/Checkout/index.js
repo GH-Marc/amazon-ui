2ce73a4a52cde2ca84f18c7c74eb22621002c94e
Marc <marcelopgm65@gmail.com>
2020-12-25 19:33:00 -0300

    :sparkles: Checkout page

---
+++ src/pages/Checkout/index.js
@@ -0,0 +1,46 @@
+import './styles.css';
+
+import Subtotal from '../../components/Subtotal';
+import CheckoutProduct from '../../components/CheckoutProduct';
+
+import { useStateValue } from '../../hooks/StateProvider';
+
+function Checkout() {
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt="banner_ad"
+        />
+
+        <div>
+          <h3>Olá, {user?.email}</h3>
+
+          <h2 className="checkout__title">
+            Seu carrinho
+          </h2>
+
+          {basket.map(item => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className="checkout__right">
+        <Subtotal />
+      </div>
+    </div>
+  );
+}
+
+export default Checkout;
