26f6a4b6ce21f2410b74afd0f2b5c33cf4f9b978
Marc <marcelopgm65@gmail.com>
2020-12-25 00:38:12 -0300

    :gear: Configs to API services

---
+++ functions/index.js
@@ -0,0 +1,29 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+const stripe = require('stripe')('sk_test_51I24oGJUrD9XVOcRLuvhteZfHZ7kext3W491vVm2dw5QnFBToPmduu5d1ern6qBox0kFzEldKP5h0j04ikbxPZ7b00ZL6hlBAp');
+
+const app = express();
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+app.get('/', (request, response) => response.status(200).send('Hello'));
+app.post('/payments/create', async (request, response) => {
+  const total = request.query.total;
+
+  console.log('Recibo de pagamento recebido', total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: 'BRL',
+  });
+
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+exports.api = functions.https.onRequest(app);
+
+// http://localhost:5001/ui-1d15d/us-central1/api
