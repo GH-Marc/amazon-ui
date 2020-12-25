6c0f98353c48cbb94df4f122fe88dc96342e99e5
Marc <marcelopgm65@gmail.com>
2020-12-25 00:44:28 -0300

    :wrench: chore: Adds Payment route +

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import { useEffect } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 
 import './App.css';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+
 import Header from './components/Header';
 import Home from './components/Home';
 import Login from './components/Login';
 import Checkout from './components/Checkout';
+import Payment from './components/Payment';
 
 import { auth } from './firebase';
 import { useStateValue } from './hooks/StateProvider';
+
+
+const promise = loadStripe('pk_test_51I24oGJUrD9XVOcR3ybU2gK94794MmlzJt6fvngO2GtnaxUXmkIvWP1viyFYphrZDJdUovYUThHoNY6nGaTMMYsA00bEibDVoV');
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -36,6 +43,13 @@ function App() {
       <div className="app">
 
         <Switch>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+          </Route>
+
           <Route path="/login">
             <Login />
           </Route>
